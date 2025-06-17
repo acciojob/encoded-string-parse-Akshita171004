f0f45be5849296e5ea8646a4e38072733115ec6a
Akshita171004 <akshitakaushik17@gmail.com>
2025-06-18 00:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const parseCode = (str) => {
-  // your code here
+  // Split by one or more zeros using regex
+  const parts = str.split(/0+/);
+  
+  // Return the required object
+  return {
+    firstName: parts[0],
+    lastName: parts[1],
+    id: parts[2]
+  };
 };
 
 // Do not change the code below
